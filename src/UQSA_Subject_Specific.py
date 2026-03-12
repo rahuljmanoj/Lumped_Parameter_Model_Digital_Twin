85893d77330d10d3be3b7972b60c04f50e53b51b
Rahul <rahulmanojktym@gmail.com>
2026-03-12 15:32:32 +0100

    UQSA file saveing path changed

--- src/UQSA_Subject_Specific.py
+++ src/UQSA_Subject_Specific.py
@@ -20,9 +20,9 @@ SHEET_NAME   = "Study_3_V4.1_T6"     # adjust if needed
 #SUBJECT_LIST = [316, 319, 323, 325, 326, 327, 328, 329, 331, 332, 334, 336, 337, 338, 339, 341, 342, 343,
                 #344, 346, 347, 349, 351, 352]
 #SUBJECT_LIST =  [354, 356, 357, 359, 360]
-#SUBJECT_LIST =  [361, 362, 363, 364, 365]
-SUBJECT_LIST =  [366, 367, 368, 369, 370, 371, 372, 382, 383, 385, 386, 387, 388, 389, 390, 391, 392, 396, 397, 398, 399, 400, 401, 402, 403, 404, 405, 409, 410, 411, 412, 413, 414, 415]                # 999 = mean row; or [316, 319, ...]
-SAVE_ROOT    = r".\Results_all_subjects_UQSA_LPM_4.1"
+#SUBJECT_LIST =  [361, 362, 363, 364, 365,366, 367, 368, 369, 370]
+SUBJECT_LIST =  [371, 372, 382, 383, 385, 386, 387, 388, 389, 390, 391, 392, 396, 397, 398, 399, 400, 401, 402, 403, 404, 405, 409, 410, 411, 412, 413, 414, 415]                # 999 = mean row; or [316, 319, ...]
+SAVE_ROOT    = r"C:\Workspace\Post_Doc_Works_NTNU\Projects\2_SWE_Velocity_LV_Filling_Pressure_Digital_Twin\3_Codes\Python\Results_all_subjects_UQSA_LPM_4.1"
 
 # Monte-Carlo (UQ)
 N_MC     = 75000          # your requested 75k
@@ -30,7 +30,7 @@ MC_MODE  = "uniform"      # "uniform" in z-window, or "normal" around z_best
 PRIOR_CV = 0.15           # only used if MC_MODE=="normal" (σ in z)
 
 # Sobol (SA)
-SOBOL_BASE_N =2048       # your requested base N
+SOBOL_BASE_N = 2048       # your requested base N
 SOBOL_BOOT   = 2000       # your requested bootstrap resamples for CI
 SECOND_ORDER = False      # can set True if you want S2 (costly)
 
